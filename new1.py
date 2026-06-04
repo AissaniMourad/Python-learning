2f214998475454d531e91756959c8b244c5e19ba
AissaniMourad <aissanimourad444@gmail.com>
2026-06-04 23:39:19 +0100

    remove new1.py

--- new1.py
+++
@@ -1,8 +0,0 @@
-# Exercise 1: Rectangle Area Calc
-
-
-length = float(input("Enter the length: "))
-width = float(input("Enter the Width: "))
-area = length*width
-
-print(f"The Area is: {area}cm²")
