f933df1b85332bcd6935f3953903701e5cc55c74
AissaniMourad <aissanimourad444@gmail.com>
2026-06-04 23:15:41 +0100

    Day 1 - area calculator

---
+++ new1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Rectangle Area Calc
+
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the Width: "))
+area = length*width
+
+print(f"The Area is: {area}cm²")
