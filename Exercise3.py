168918017d830f4a22fd1cb09f8f8549349da876
AissaniMourad <aissanimourad444@gmail.com>
2026-06-05 11:52:35 +0100

    Day 2 - area of circle calculator

---
+++ Exercise3.py
@@ -0,0 +1,9 @@
+# Execise Calculate the Area of a Circle
+
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+
+area = math.pi*pow(radius, 2)
+
+print(f"The area is: {round(area, 2)}cm²")
