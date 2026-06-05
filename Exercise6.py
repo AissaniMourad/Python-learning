941b7b97473a3b60a7260a4497e3c404c9031692
AissaniMourad <aissanimourad444@gmail.com>
2026-06-05 12:34:07 +0100

    Day 2 - Python weight converter

---
+++ Exercise6.py
@@ -0,0 +1,15 @@
+# Exercise python Weight Converter
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds? (K or L): ")
+
+if unit.lower() == "k":
+    weight *= 2.205
+    unit = "Lbs."
+    print(f"Your weight is {round(weight, 1)} {unit}")
+elif unit.lower() == "l":
+    weight /= 2.205
+    unit = "Kgs."
+    print(f"Your weight is {round(weight, 1)} {unit}")
+else:
+    print(f"{unit} was not valid")
