d1b11b11b038935f4ab59b8650658274a1c09887
AissaniMourad <aissanimourad444@gmail.com>
2026-06-07 21:31:57 +0100

    Day 4 - Python Number Guessing Game

--- Exercise15.py
+++ Exercise15.py
@@ -1,7 +1,34 @@
-cart = []
+# Exercise Number guessing game
 
+import random
 
-food = input("Enter a food: ")
-cart.append(food)
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
 
-print(cart)
+print("------ Number Geussing Game ------")
+print(f"Setect a number between {lowest_num} and {highest_num}: ")
+
+while is_running:
+    guess = input("Enter your guess: ")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of rang")
+            print(
+                f"Please setect a number between {lowest_num} and {highest_num}: ")
+        elif guess < answer:
+            print("Too low! Try again")
+        elif guess > answer:
+            print("Too high! Try again")
+        else:
+            print(f"CORRECT! The answer was: {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+    else:
+        print("Invalide guess")
+        print(
+            f"Please setect a number between {lowest_num} and {highest_num}: ")
