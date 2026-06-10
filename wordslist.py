9ae9bae0a7bc9f1a75d74c126e7aa301648fc8c1
AissaniMourad <aissanimourad444@gmail.com>
2026-06-10 19:07:50 +0100

    Day 7 - Python Hangman Game

---
+++ wordslist.py
@@ -0,0 +1,21 @@
+# words for hangman game
+
+words = (
+    "cat", "dog", "fox", "cow", "pig", "hen", "bat", "ant", "emu", "owl",
+    "bee", "elk", "ram", "yak", "asp", "cod", "gnu", "koi", "mew", "jay",
+
+    "rabbit", "parrot", "jaguar", "toucan", "falcon", "donkey", "giraffe",
+    "hamster", "leopard", "penguin", "gorilla", "dolphin", "octopus",
+    "ostrich", "peacock",
+
+    "chameleon", "crocodile", "chimpanzee", "salamander", "barracuda",
+    "wolverine", "narwhal", "platypus", "armadillo", "rhinoceros",
+    "orangutan", "roadrunner", "porcupine", "axolotl", "cockatoo",
+
+    "chameleon", "crocodile", "chimpanzee", "salamander", "barracuda",
+    "wolverine", "narwhal", "platypus", "armadillo", "rhinoceros",
+    "orangutan", "roadrunner", "porcupine", "axolotl", "cockatoo",
+    "tarantula", "albatross", "alligator", "bumblebee", "centipede",
+    "dragonfly", "flamingo", "hedgehog", "jellyfish", "kingfisher",
+    "manatee", "mongoose", "pangolin", "scorpion", "secretary bird",
+)
