f972d711ef69b0e337af5f4b699c9fa30adce34e
AissaniMourad <aissanimourad444@gmail.com>
2026-06-13 18:57:31 +0100

    Day 9 - Python PyQt5 Layouts

---
+++ Exercise28.py
@@ -0,0 +1,49 @@
+# Exercise PyQt5 layouts
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+                             QWidget, QVBoxLayout, QHBoxLayout, QGridLayout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(400, 150, 500, 500)
+        self.initUI()
+
+    def initUI(self):
+        central_weidget = QWidget()
+        self.setCentralWidget(central_weidget)
+
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
+
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: yellow;")
+        label3.setStyleSheet("background-color: green;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
+
+        grid = QGridLayout()
+
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(label2, 0, 1)
+        grid.addWidget(label3, 1, 0)
+        grid.addWidget(label4, 1, 1)
+        grid.addWidget(label5, 1, 2)
+
+        central_weidget.setLayout(grid)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
