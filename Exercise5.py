cadb2ff749926a499950037a6f262dcbe46e7c49
AissaniMourad <aissanimourad444@gmail.com>
2026-06-14 23:56:47 +0100

    Day 2 - Python calculator (UPDATE)

--- Exercise5.py
+++ Exercise5.py
@@ -1,5 +1,7 @@
 # Exercise : Python Calculator
 
+is_running = True
+
 operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
@@ -18,3 +20,26 @@ elif operator == "/":
     print(round(result, 3))
 else:
     print(f"{operator} is not a valid operator")
+
+while is_running:
+    done = input("Do you wana cantinue the calculations? (Y/N): ").upper()
+    if not done == "Y":
+        print(f"The total is: {result}")
+        is_running = False
+    else:
+        operator = input("Enter an operator (+ - * /): ")
+        num2 = float(input("Enter the 2nd number: "))
+        if operator == "+":
+            result += num2
+            print(round(result, 3))
+        elif operator == "-":
+            result -= num2
+            print(round(result, 3))
+        elif operator == "*":
+            result *= num2
+            print(round(result, 3))
+        elif operator == "/":
+            result /= num2
+            print(round(result, 3))
+        else:
+            print(f"{operator} is not a valid operator")
